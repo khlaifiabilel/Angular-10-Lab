64f8b727d77b93454dc93f5e80c96293116d72d5
Kalifia <Billal>
2020-11-20 09:46:19 +0100

    New components where added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
+import { AboutComponent } from './about/about.component';
+import { ListComponent } from './list/list.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home' , pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    HelpComponent,
+    AboutComponent,
+    ListComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
